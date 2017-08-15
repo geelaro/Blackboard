21e98b9903c0f26857f30b379bbce4a2265f7ba6
geelaro <leegzone@gmail.com>
2017-08-16 00:17:40 +0800

    8.16 add AboutFragment

---
+++ app/src/main/java/com/geelaro/sunshine/about/AboutFragment.java
@@ -0,0 +1,20 @@
+package com.geelaro.sunshine.about;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by LEE on 2017/8/15.
+ */
+
+public class AboutFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,  ViewGroup container, Bundle savedInstanceState) {
+
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
