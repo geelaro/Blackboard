591558783da088007e81fa7650604f8921acb4cc
Brian Lee <brian@BriandeMacBook-Pro.local>
2017-12-31 00:21:47 +0800

    12.31 添加中英文切换

---
+++ app/src/main/java/com/geelaro/sunshine/settings/Settings.java
@@ -0,0 +1,41 @@
+package com.geelaro.sunshine.settings;
+
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import com.geelaro.sunshine.utils.SunshineApp;
+
+/**
+ * Created by brian on 2017/12/30.
+ */
+
+public class Settings {
+
+    public final static String LANGUAGE_LIST = "language_list";
+
+    public static boolean needRecreate;
+
+    private static volatile Settings mInstance;
+
+    private SharedPreferences mPrefs;
+
+    private Settings() {
+        mPrefs = PreferenceManager.getDefaultSharedPreferences(SunshineApp.getContext());
+    }
+
+    public synchronized static Settings getInstance() {
+        if (mInstance == null) {
+            synchronized (Settings.class) {
+                if (mInstance == null) {
+                    mInstance = new Settings();
+                }
+            }
+        }
+        return mInstance;
+
+    }
+
+    public String getString(String key, String value) {
+        return mPrefs.getString(key, value);
+    }
+}
